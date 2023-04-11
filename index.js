721f2aa30f7ce3ed131c4264ad60ef5ce2c4b4c7
Prathmesh-probosys <69837600+Prathmesh-probosys@users.noreply.github.com>
2023-04-11 16:19:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ const app = express()
  
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+    var port = 5000;
 app.get('/processGet', (req, res) => {
-  res.send('Hello World!')
+	const date = "1996-07-04T00:00:00" //req.body;
+	console.log(date);
+	const dateObject = new Date(date);
+	console.log(dateObject);
+	var formattedDate = dateObject.getDate() + '/' + (dateObject.getMonth()+1) + '/' + (dateObject.getFullYear()-1);
+  res.send('Hello World!' + formattedDate);
 })
  
 app.post('/processdate', (req, res) => {
-    let data = req.body;
+   let  data = "1996-07-04T00:00:00" //req.body;
     res.send('Data Received: ' + JSON.stringify(data));
 })
  
  
-app.listen(5000, () => {
-  console.log('Example app listening on port 3000!')
-})
+ app.listen(port, function(){
+            console.log('Server is running at port: ',port);
+          });
